657ac727b90c62eb93ba821bf3a57f90ec9b4331
hokuseihal <hokuseihal@gmail.com>
2021-10-01 20:31:55 +0900

    add tfrecord make

--- utils/tfrecord.py
+++ utils/tfrecord.py
@@ -3,7 +3,7 @@ import os
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 import tensorflow as tf
 import torch
-
+import glob
 
 
 class TFRDataloader():
@@ -34,19 +34,56 @@ class TFRDataloader():
         self.m = m
         self.s = s
 
-
     def __iter__(self):
         return self
 
     def __next__(self):
-        return (torch.from_numpy(next(self.tfdataset)) - self.m) / self.s
+        return torch.from_numpy(next(self.tfdataset) - self.m) / self.s
 
     def __len__(self):
         return 202589 // self.batch
 
 
+def maketfrecord(re_imgpath, tfrpath):
+    def _bytes_feature(value):
+
+        if isinstance(value, type(tf.constant(0))):
+            value = value.numpy()  # BytesList won't unpack a string from an EagerTensor.
+        return tf.train.Feature(bytes_list=tf.train.BytesList(value=[value]))
+
+    def _float_feature(value):
+        return tf.train.Feature(float_list=tf.train.FloatList(value=[value]))
+
+    def _int64_feature(value):
+        return tf.train.Feature(int64_list=tf.train.Int64List(value=[value]))
+
+    def serialize_example(image_string):
+        feature = {
+
+            'image': _bytes_feature(image_string),
+        }
+
+        example_proto = tf.train.Example(features=tf.train.Features(feature=feature))
+        return example_proto.SerializeToString()
+    with tf.io.TFRecordWriter(tfrpath) as writer:
+        for path in glob.glob(re_imgpath)[:10]:
+            print(path)
+            with open(path, 'rb') as img:
+                img_str=img.read()
+            writer.write(serialize_example(img_str))
+
+
 if __name__ == '__main__':
     # tfdataset=tf.data.TFRecordDataset(path).map(_parse_image_function).as_numpy_iterator()
-    path = '/home/hokusei/src/data/celeba.tfrecord'
-    for i, x in enumerate(TFRDataloader(path=path, size=128, epoch=1, batch=4096,s=1,m=0)):
-        print(x.permute(0,3,2,1).reshape(-1,3).mean(0))
+    maketfrecord('/home/hokusei/src/data/celeba/img_align_celeba/*.jpg', 'tmp.tfrecord')
+    # path = 'tmp.tfrecord'
+    # for i, x in enumerate(TFRDataloader(path=path, size=128, epoch=1, batch=1,s=1,m=0)):
+    #     print(x.permute(0,3,2,1).reshape(-1,3).mean(0))
+    #
+    # for example_proto in tf.data.TFRecordDataset('tmp.tfrecord').as_numpy_iterator():
+    #     parsed_example = tf.io.parse_single_example(example_proto, {'image': tf.io.FixedLenFeature([], tf.string)})
+    #     image = tf.image.decode_jpeg(parsed_example['image'], channels=3)
+    #     image = tf.cast(image, tf.float32) / 255.0
+    #     image=image.numpy()
+    #     image=torch.from_numpy(image)
+    #     print(image.dtype)

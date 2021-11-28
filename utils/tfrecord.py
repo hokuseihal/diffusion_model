9cca43ceb71d5ed45c481bc49325396c586a69cd
hokuseihal <hokuseihal@gmail.com>
2021-11-28 14:08:48 +0900

    add index tfrecord

--- utils/tfrecord.py
+++ utils/tfrecord.py
@@ -3,26 +3,28 @@ import os
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 import tensorflow as tf
 import torch
-import glob
 
 
 class TFRDataloader():
     def _parse_image_function(self, example_proto):
-        parsed_example = tf.io.parse_single_example(example_proto, {'image': tf.io.FixedLenFeature([], tf.string)})
+        parsed_example = tf.io.parse_single_example(example_proto, {
+            'image': tf.io.FixedLenFeature([], tf.string),
+            'index': tf.io.FixedLenFeature([], tf.int64)
+        })
 
         image = tf.image.decode_jpeg(parsed_example['image'], channels=3)
+        index = parsed_example['index']
         image = tf.cast(image, tf.float32) / 255.0
-
         if self.size is not None:
             s = tf.shape(image)
             minsize = tf.minimum(s[0], s[1])
             image = tf.image.resize_with_crop_or_pad(image, minsize, minsize)
             image = tf.image.resize(image, [self.size, self.size])
         image = tf.transpose(image, [2, 0, 1])
-        return image
+        return image, index
 
-    def __init__(self, path,batch, s, m, size=None):
-        self.path=path
+    def __init__(self, path, batch, s, m, size=None):
+        self.path = path
         self.size = size
         self.batch = batch
         self.m = m
@@ -41,7 +43,8 @@ class TFRDataloader():
 
     def __next__(self):
         try:
-            return torch.from_numpy(next(self.tfdataset) - self.m) / self.s
+            img, index = next(self.tfdataset)
+            return torch.from_numpy(img - self.m) / self.s, index
         except StopIteration:
             self.setdataset()
             raise StopIteration
@@ -50,7 +53,7 @@ class TFRDataloader():
         return 202589 // self.batch
 
 
-def maketfrecord(re_imgpath, tfrpath):
+def maketfrecord(txtpath, tfrpath):
     def _bytes_feature(value):
 
         if isinstance(value, type(tf.constant(0))):
@@ -63,31 +66,35 @@ def maketfrecord(re_imgpath, tfrpath):
     def _int64_feature(value):
         return tf.train.Feature(int64_list=tf.train.Int64List(value=[value]))
 
-    def serialize_example(image_string):
+    def serialize_example(image_string, index):
         feature = {
-
             'image': _bytes_feature(image_string),
+            'index': _int64_feature(index)
         }
 
         example_proto = tf.train.Example(features=tf.train.Features(feature=feature))
         return example_proto.SerializeToString()
 
+    with open(txtpath) as f:
+        pathes = f.readlines()
     with tf.io.TFRecordWriter(tfrpath) as writer:
-        for path in glob.glob(re_imgpath):
+        for path in pathes:
+            path = path.strip()
             print(path)
+            index = int(path.split('/')[-1].split('.')[0])
             with open(path, 'rb') as img:
                 img_str = img.read()
-            writer.write(serialize_example(img_str))
+            writer.write(serialize_example(img_str, index))
 
 
 if __name__ == '__main__':
     # tfdataset=tf.data.TFRecordDataset(path).map(_parse_image_function).as_numpy_iterator()
-    # maketfrecord('/home/hokusei/src/data/celeba/img_align_celeba/*.jpg', 'tmp.tfrecord')
+    maketfrecord('train.txt', 'tmp.tfrecord')
     # path = 'tmp.tfrecord'
     # for i, x in enumerate(TFRDataloader(path=path, size=128, epoch=1, batch=1,s=1,m=0)):
     #     print(x.permute(0,3,2,1).reshape(-1,3).mean(0))
     #
-    loader=TFRDataloader('../data/celeba.tfrecord',1,2048,0,0)
-    for e in range(2):
-        for idx,x in enumerate(loader):
-            print(e,idx)
+    # loader=TFRDataloader('../data/celeba.tfrecord',1,2048,0,0)
+    loader = TFRDataloader('tmp.tfrecord', batch=64, s=1, m=0, size=128)
+    for idx, x in enumerate(loader):
+        print(x)
